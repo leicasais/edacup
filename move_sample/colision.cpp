4a790137b224283c72b7ecbb06cf263fb15ee24b
leicasais <lcasais@itba.edu.ar>
2025-11-09 17:20:33 -0300

    push

--- move_sample/colision.cpp
+++ move_sample/colision.cpp
@@ -1,27 +1,7 @@
-#ifndef LIBS
-#define LIBS
-#include <exception>
-#include <cmath>
-#include <iostream>
-#include <string>
-#endif
-
-struct Field {
-    float halfX = 219.0 / 2.0; // eje largo (109.5 cm)
-    float halfZ = 158.0 / 2.0; // eje corto (79 cm)
-    float safety = 9.0 + 1.0;  // radio robot (9cm) + 1 cm extra
-};
-
-// Áreas de penalti: 25 cm de ancho (±12.5 desde centro) y 80 cm de largo
-struct Penalty {
-    float Width = 25.0;  // largo en x de cda porteria
-    float depth = 80.0;             // 80 cm de profundidad desde la portería
-    float robotRadius = 9.0;     
-    float safetyMargin = 1.0;
-};
+#include "colision.hpp"
 
 // Limita una coordenada al rectángulo de juego con margen de seguridad
-inline void clampToField(float& x, float& z, const Field& f) {
+void clampToField(float& x, float& z, const Field& f) {
     const float maxX = f.halfX - f.safety;
     const float maxZ = f.halfZ - f.safety;
     if (x >  maxX) x =  maxX;
